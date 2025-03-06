c64367c0a72798628505f2356ff45828d37d1d18
ValeriaCA9 <vale38646@gmail.com>
2025-03-06 14:03:33 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,14 @@
+//Cruz Alegria Valeria Montserrat
+//ejercicio1
+#include <stdio.h>
 
+int main()
+{
+    int a=3;
+    int b=2;
+    if(a>b)
+    {
+        printf("a es mayor\n");
+    }
+    return 0;
+}
