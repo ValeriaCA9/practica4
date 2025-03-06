c7b74f931c3cae576e71c5d77572d6e08023b15e
ValeriaCA9 <vale38646@gmail.com>
2025-03-06 14:17:00 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,18 @@
+//Cruz Alegria Valeria Montserrat
+//ejercicio1
+#include <stdio.h>
+
+int main()
+{
+    int a=3;
+    int b=4;
+    if(a>b)
+    {
+        printf("a es mayor\n");
+    }
+    else
+    {
+        printf("b es mayor o igual a \n");
+    }
+    return 0;
+}
